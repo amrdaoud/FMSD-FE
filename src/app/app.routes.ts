8da60ad3b08ea9811bb2c484102dcaf7b0dbb138
amr-daoud <amr.f.daoud@gmail.com>
2024-11-08 18:53:26 +0300

    #FMS-23: Add dashboard layout base and route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', pathMatch: 'full', redirectTo: 'dashboard'},
+    {path: 'dashboard', loadComponent: () => import('./dashboards/dashboard-layout/dashboard-layout.component').then(c => c.DashboardLayoutComponent)}
+];
