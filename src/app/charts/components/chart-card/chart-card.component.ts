f7ea0d933f1fd1e9bdfbb0163932c9960610648c
amr-daoud <amr.f.daoud@gmail.com>
2024-11-08 18:39:13 +0300

    #FMS-27: Create chart component

--- src/app/charts/components/chart-card/chart-card.component.ts
+++ src/app/charts/components/chart-card/chart-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input, model, signal } from '@angular/core';
+import { ChartConfiguration } from 'chart.js';
 
 @Component({
   selector: 'app-chart-card',
@@ -8,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './chart-card.component.scss'
 })
 export class ChartCardComponent {
-
+  options = input<string[]>([]);
+  selectedOption = signal<number>(0);
+  title = input<string>('Fuel Availability');
+  subtitle = input<string>('Total fuel availability over strategic stations (click to drill down)');
+  drillParameter = model<{index: number, label: string, parentLabel: string}[]>([]);
+  values = input<{name: string, value: string}[][]>([]);
+  stopindex = input<number>(2);
 }
